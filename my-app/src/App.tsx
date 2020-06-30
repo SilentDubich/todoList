40c9ed361d5de9804ab81d9bf24018f3cafd92fb
SilentDubich <kirill.dubov.2012@mail.ru>
2020-06-30 21:04:42 +0300

    Added localstorage for tasks

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,21 +1,31 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {TodoForm} from "./Components/todoForm";
 import {TaskType, TodoList} from "./Components/todoList";
 
 function App() {
-    const [tasks, setTasks] = useState<Array<TaskType>>([{id: 1, body: 'Hello world !', isCompleted: false}])
-    const [idCounter, setIdCounter] = useState(2)
+    let storage: any = []
+    let id: any = 0
+    storage = localStorage.tasks && localStorage.getItem('tasks')
+    id = localStorage.id && localStorage.getItem('id')
+    if (storage) storage = JSON.parse(storage)
+    if (id) id = +id
+    const [tasks, setTasks] = useState<Array<TaskType>>(storage)
+    const [idCounter, setIdCounter] = useState(id)
     const addTask = (task: string) => {
         setTasks([...tasks, {id: idCounter, body: task, isCompleted: false}])
         setIdCounter(idCounter + 1)
+        localStorage.setItem('tasks', JSON.stringify([...tasks, {id: idCounter, body: task, isCompleted: false}]))
+        localStorage.setItem('id', (idCounter + 1).toString())
     }
     const deleteTask = (id: number) => {
         setTasks([...tasks.filter( el => el.id !== id)])
+        localStorage.setItem('tasks', JSON.stringify([...tasks.filter( el => el.id !== id)]))
     }
     const setComplete = (id: number, isComplete: boolean) => {
         setTasks(tasks.map(el => el.id === id ? {id: el.id, body: el.body, isCompleted: isComplete} : el))
+        localStorage.setItem('tasks', JSON.stringify(tasks.map(el => el.id === id ? {id: el.id, body: el.body, isCompleted: isComplete} : el)))
     }
   return (
     <div>
